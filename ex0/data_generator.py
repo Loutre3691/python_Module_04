373894ad182e1e2d99f03557ab9d23e230ecb11c
Flora De Checchi <flora.dechecchi@gmail.com>
2026-03-28 18:05:04 +0100

    ex1 fini

---
+++ ex0/data_generator.py
@@ -0,0 +1,9 @@
+
+if __name__ == "__main__":
+    file = open("ancient_fragment.txt", "w")
+    file.write("RECOVERED DATA:\n")
+    file.write("[FRAGMENT 001]: Digital preservation protocols established \
+2087\n")
+    file.write("[FRAGMENT 002]: Knowledge must survive the entropy wars\n")
+    file.write("[FRAGMENT 003]: Every byte saved is a victory against oblivion")
+    file.close()
---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,18 @@
+
+
+if __name__ == "__main__":
+    print("\033[1;35m\n==== CYBER ARCHIVES - DATA RECOVERY SYSTEM "
+          "===\033[0m\n")
+    try:
+        file = open("ancient_fragment.txt")
+        print(f"Accessing storage Vault: {file.name}")
+        print("connection established...\n")
+        print(file.read())
+
+    except FileNotFoundError:
+        print("\033[0;31mError: Storage vault not found\033\n[0m")
+        exit()
+
+    print(file.read())
+    print("\nData recovery complete. Storage unit disconnected.")
+    file.close()
