3fb4d929a7f94df6c628418e1b91545ccafb1a06
flora <flora.dechecchi@gmailcom>
2026-03-30 23:42:18 +0200

    ex4 fini

--- ex4/ft_crisis_response.py
+++ ex4/ft_crisis_response.py
@@ -1,12 +1,8 @@
-
-if __name__ == "__main__":
-    print("\033[1;35m\n==== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM "
-          "===\033[0m\n")
-
-    print("\033[1;33mCRISIS ALERT: Attempting access to 'lost_archive.txt' "
+def crisis_handler(fichier: str) -> None:
+    print(f"\033[1;33mCRISIS ALERT: Attempting access to {fichier}"
           ".\033[0m")
     try:
-        with open("lost_archive.txt", "r") as f:
+        with open(fichier, "r") as f:
             print(f.read())
             print("STATUS: Crisis handled, system stable\n")
 
@@ -18,25 +14,13 @@ if __name__ == "__main__":
         print("RESPONSE: Security protocols deny access")
         print("STATUS: Crisis handled, security maintained\n")
 
-    print("\033[1;33mCRISIS ALERT: Attempting access to 'classified_vault.txt'"
-          "...\033[0m")
+
+def routine_access(fichier: str) -> None:
+    print(f"\033[1;33mROUTINE ACCESS: Attempting access to "
+          f"{fichier}...\033[0m")
+
     try:
-        with open("classified_vault.txt", "r") as f:
-            print(f.read())
-            print("STATUS: Crisis handled, system stable\n")
-
-    except FileNotFoundError:
-        print("RESPONSE: Archive not found in storage matrix")
-        print("STATUS: Crisis handled, security maintained\n")
-
-    except PermissionError:
-        print("RESPONSE: Security protocols deny access")
-        print("STATUS: Crisis handled, security maintained\n")
-
-    print("\033[1;33mROUTINE ACCESS: Attempting access to "
-          "'standard_archive.txt'...\033[0m")
-    try:
-        with open("standard_archive.txt", "r") as f:
+        with open(fichier, "r") as f:
             print(f"SUCCESS: Archive recovered - {f.read()}")
             print("STATUS: Normal operations resumed\n")
 
@@ -47,6 +31,15 @@ if __name__ == "__main__":
     except PermissionError:
         print("RESPONSE: Security protocols deny access")
         print("STATUS: Crisis handled, security maintained\n")
+
+
+if __name__ == "__main__":
+    print("\033[1;35m\n==== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM "
+          "===\033[0m\n")
+
+    crisis_handler("lost_archive.txt")
+    crisis_handler("classified_vault.txt")
+    routine_access("standard_archive.txt")
 
     print("\033[1;35mAll crisis scenarios handled successfully. Archives "
           "secure.\033[0m")
